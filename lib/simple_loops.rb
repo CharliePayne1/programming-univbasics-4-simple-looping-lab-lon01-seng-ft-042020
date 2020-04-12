0ecdcbbcb53db270b775c0c946827d85d9e03171
Charlie Payne <charliepayne1101@gmail.com>
2020-04-12 16:51:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,7 +22,7 @@ def output_array(array)
    counter = 0 
    new_array = []
   while counter < array.length do
-    new_array = array.push[counter]
+    new_array.push(array[counter].to_s
     counter += 1
     puts new_array
 end
